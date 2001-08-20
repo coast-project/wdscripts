297dadb5f62c580813479512ca372df630672113
Marcel Huber <marcel.huber@hsr.ch>
2001-08-20 17:58:06 +0000

    removed creation of log directory

--- prjinstall.sh
+++ prjinstall.sh
@@ -12,20 +12,3 @@
 # check install.sh for an overview of pre defined variables
 
 cd $INSTALLDIRABS
-
-# creating log directories
-if [ -d log/rotate ]; then
-	echo "log/rotate dir exists"
-else
-	printf "creating log/rotate directory... "
-	mkdir -p log/rotate
-	if [ $? -ne 0 ]; then
-		printf "failed\n"
-		exit 1
-	else
-		printf "done\n"
-	fi
-fi
-
-echo "changing owner to nobody for log directory"
-chown -R nobody:other log
