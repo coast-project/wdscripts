f756b86b7da5868321226aee8db4d8cfc9cbfd5d
ham <ham>
2002-05-13 16:04:46 +0000

    * 3.9 zeugs entfernt

--- setConfig.sh
+++ setConfig.sh
@@ -2,6 +2,28 @@
 # 
 # sets the Config to first param or the project Default (set DEF_CONF in prjconfig.sh)
 
+case $2 in
+'39')
+	if [ ! -d config ]; then
+		if [ -d 39config ]; then
+			mv 39config config
+		else
+			echo "ERROR!!!!!!! keine 39er config vorhanden!!!!!"
+			exit 1
+		fi
+	fi
+	;;
+'40')
+	if [ ! -d config40 ]; then
+		echo "ERROR!!!!!!! keine 40er config gefunden!!!!!"
+		exit 1
+	fi
+	if [ -d config ]; then	
+		mv config 39config
+	fi
+	;;
+esac
+
 . `dirname $0`/config.sh
 
 CONF=${1:-$DEF_CONF}
@@ -11,6 +33,7 @@ $SCRIPTDIR/editConfig.sh $CONFIGDIR any $LOGDIR/edit.log 0 $CONF "$ALL_CONFIGS"
 $SCRIPTDIR/editConfig.sh $CONFIGDIR sh $LOGDIR/edit.log 0 $CONF "$ALL_CONFIGS" 
 $SCRIPTDIR/editConfig.sh $PROJECTDIR/src sh $LOGDIR/editsh.log 0 $CONF "$ALL_CONFIGS"
 $SCRIPTDIR/editConfig.sh $PROJECTDIR/src pl $LOGDIR/editpl.log 0 $CONF "$ALL_CONFIGS" 
+$SCRIPTDIR/editConfig.sh $PROJECTDIR/src pm $LOGDIR/editpl.log 0 $CONF "$ALL_CONFIGS" 
 $SCRIPTDIR/editConfig.sh $PROJECTDIR/FunkTest sql $LOGDIR/editsql.log 0 $CONF "$ALL_CONFIGS" 
 $SCRIPTDIR/editConfig.sh $PROJECTDIR/FunkTest sh $LOGDIR/editsh.log 0 $CONF "$ALL_CONFIGS" 
 $SCRIPTDIR/editConfig.sh $PROJECTDIR/FunkTest/config any $LOGDIR/editany.log 0 $CONF "$ALL_CONFIGS" 
