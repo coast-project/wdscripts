d84494155019de6cfe18bfe3cb6183f513e85a78
Marcel Huber <marcel.huber@hsr.ch>
2006-08-08 17:55:17 +0000

    * made creation of log/rotate directory dependant on cfg_doLog flag

--- prjinstall.sh
+++ prjinstall.sh
@@ -14,19 +14,18 @@
 
 cd $cfg_path
 
-# creating log directories
-if [ -d ${LOGDIR}/rotate ]; then
-	echo "${LOGDIR}/rotate dir exists"
-else
-	printf "creating log/rotate directory... "
-	mkdir -p ${LOGDIR}/rotate
-	if [ $? -ne 0 ]; then
-		printf "failed\n"
-		exit 1
-	else
-		printf "done\n"
+if [ $cfg_doLog -eq 1 ]; then
+	# creating log directories
+	if [ -n "${LOGDIR}" -a ! -d ${LOGDIR}/rotate ]; then
+		printf "creating ${LOGDIR}/rotate directory... "
+		mkdir -p ${LOGDIR}/rotate
+		if [ $? -ne 0 ]; then
+			printf "failed\n"
+			exit 1
+		else
+			printf "done\n"
+		fi
 	fi
+##	echo "changing owner to nobody for log directory"
+##	chown -R nobody:other ${LOGDIR}
 fi
-
-echo "changing owner to nobody for log directory"
-chown -R nobody:other ${LOGDIR}
