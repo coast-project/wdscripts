63bf901bcb4aee5d48ffecf1ac6ece3cdabe92d6
Marcel Huber <m1huber@hsr.ch>
2009-12-04 08:02:10 +0100

    corrected expire flag
    only redirecting stdout

--- git-delete-history.sh
+++ git-delete-history.sh
@@ -16,11 +16,11 @@ if [ ! -d .git ]; then
     exit 1
 fi
 
-exec >`basename $0`.out 2>`basename $0`.err
+exec >`basename $0`.out
 # remove all paths passed as arguments from the history of the repo
 files=$@
 git filter-branch --index-filter "git rm -rf --cached --ignore-unmatch $files" HEAD
 
 # remove the temporary history git-filter-branch otherwise leaves behind for a long time
-rm -rf .git/refs/original/ && git reflog expire --all &&  git gc --aggressive --prune
+rm -rf .git/refs/original/ && git reflog expire --expire="now" --all &&  git gc --aggressive --prune
 
