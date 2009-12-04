36aebd028c65a85c3a921d02dc015207d7cc0bb9
Marcel Huber <marcel.huber@hsr.ch>
2009-12-05 00:54:23 +0100

    improved usability of git helper scripts

--- git-delete-history.sh
+++ git-delete-history.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
-set -o errexit
 
-# Author: David Underhill
-# Script to permanently delete files/folders from your git repository.  To use 
-# it, cd to your repository's root and then run the script with a list of paths
-# you want to delete, e.g., git-delete-history path1 path2
+USAGE="file_or_dir [another_file_or_dir...]"
+LONG_USAGE=""
 
-if [ $# -eq 0 ]; then
-    exit 0are still
-fi
+OPTIONS_SPEC=
+. `dirname $0`/git-sh-setup
+require_work_tree
 
-# make sure we're at the root of git repo
-if [ ! -d .git ]; then
-    echo "Error: must run this script from the root of a git repository"
-    exit 1
-fi
+##exec >`basename $0`.out # disabled due to using git rm -q
 
-exec >`basename $0`.out
 # remove all paths passed as arguments from the history of the repo
-files=$@
-git filter-branch --tag-name-filter cat --index-filter "git rm -rf --cached --ignore-unmatch $files" --commit-filter 'git_commit_non_empty_tree "$@"' HEAD
+index_filter=$(cat <<- EOF
+git rm -q -rf --cached --ignore-unmatch $@
+EOF
+)
+
+commit_filter=$(cat <<- EOF
+git_commit_non_empty_tree "\$@"
+EOF
+)
+
+cmd="git filter-branch --tag-name-filter cat --index-filter '${index_filter}' --commit-filter '${commit_filter}' -- HEAD"
+echo ${cmd}
+echo "Continue (*y|n)?"
+read yesno
+if [ "$yesno" = "n" -o "$yesno" = "N" ]; then
+  exit 3;
+fi
+eval ${cmd}
 
 # remove the temporary history git-filter-branch otherwise leaves behind for a long time
 rm -rf .git/refs/original/ && git reflog expire --expire="now" --all &&  git repack -a -d && git gc --aggressive --prune
--- git-move-files.sh
+++ git-move-files.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
-set -o errexit
 
-if [ $# -eq 0 ]; then
-    exit 0are still
+USAGE="\"srch_path_sedArg1\" \"repl_path_sedArg2\" [more s/r tuples]"
+LONG_USAGE="bla"
+
+OPTIONS_SPEC=
+. `dirname $0`/git-sh-setup
+require_work_tree
+
+##exec >`basename $0`.out
+
+testeven() {
+if [ $1 -eq 1 ]; then
+	echo "uneven number of arguments given, exiting!"
+	usage;
 fi
+}
 
-# make sure we're at the root of git repo
-if [ ! -d .git ]; then
-    echo "Error: must run this script from the root of a git repository"
-    exit 1
-fi
-
-#exec >`basename $0`.out
+testeven $#
 # remove all paths passed as arguments from the history of the repo
-srch="${1}"
-repl="${2:-}"
+sed_expression=""
+
+while [ $# -ge 2 ]; do
+	srch="${1}"
+	repl="${2}"
+	sed_expression="${sed_expression} -e \"s-\t${srch}-\t${repl}-\""
+	shift 2
+	testeven $#
+done
 
 index_filter=$(cat <<- EOF
-git ls-files -s | sed "s-\t${srch}-\t${repl}-" | \
+git ls-files -s | sed ${sed_expression} | \
 	GIT_INDEX_FILE=\${GIT_INDEX_FILE}.new \
 	git update-index --index-info &&
 	mv \${GIT_INDEX_FILE}.new \${GIT_INDEX_FILE}
@@ -25,6 +37,11 @@ EOF
 )
 cmd="git filter-branch --tag-name-filter cat --index-filter '${index_filter}' -- HEAD"
 echo ${cmd}
+echo "Continue (*y|n)?"
+read yesno
+if [ "$yesno" = "n" -o "$yesno" = "N" ]; then
+  exit 3;
+fi
 eval ${cmd}
 
 # remove the temporary history git-filter-branch otherwise leaves behind for a long time
