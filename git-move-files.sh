82e2fd48f4ccfac8c88bfdce57cfd4d04d9f3a21
Marcel Huber <marcel.huber@hsr.ch>
2010-01-30 13:55:14 +0100

    added revision option to limit range of replacement
    improved help text

--- git-move-files.sh
+++ git-move-files.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
-USAGE="\"srch_path_sedArg1\" \"repl_path_sedArg2\" [more s/r tuples]"
+USAGE=""
 LONG_USAGE=""
 
-OPTIONS_SPEC=
+OPTIONS_SPEC="\
+$(basename "$0") [options] \"search\" \"replace\"...
+
+The search/replace tuples will be used within --index-filter to rename filenames
+in the index. For every commit, the content of the index will be piped to sed using
+git ls-files -s . Specified search and replace tokens will be used to form sed
+expressions of the form 's-\t\${search}-\t\${replace}-'.
+You can use any sed expression and replacement tokens (like \1) but keep in mind
+to escape special characters like ()+?
+--
+h,help show the help
+dorevs= revisions to work on, default is --all, use something like HEAD~10..HEAD to only work on last 10 commits
+"
+
 . "$(git --exec-path)/git-sh-setup"
 require_work_tree
 
+revs="--all "
+while true; do
+	case "$1" in
+		--dorevs) revs="${revs} $2"; shift 2;;
+		--)	shift; break;;
+		*) break;;
+	esac
+done
+
 testeven() {
 if [ $1 -eq 1 ]; then
-	echo "uneven number of arguments given, exiting!"
+	echo "uneven number of search/replace arguments given, exiting!"
 	usage;
 fi
 }
@@ -36,7 +58,7 @@ EOF
 
 cmd="git filter-branch --tag-name-filter cat"
 cmd="${cmd} --index-filter '${index_filter}'"
-cmd="${cmd} -- --all"
+cmd="${cmd} -- ${revs}"
 
 echo ${cmd}
 echo "Continue (*y|n)?"
@@ -46,7 +68,12 @@ if [ "$yesno" = "n" -o "$yesno" = "N" ]; then
 fi
 eval ${cmd}
 
-# remove the temporary history git-filter-branch otherwise leaves behind for a long time
-git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref -d
-git reflog expire --expire=now --all &&  git repack -ad && git gc --aggressive --prune=now
+cmdCode=$?
+echo "retcode of command ${cmdCode}"
+
+if [ ${cmdCode} -eq 0 ]; then
+	# remove the temporary history git-filter-branch otherwise leaves behind for a long time
+	git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref -d 2>/dev/null || exit 0
+	git reflog expire --expire=now --all &&  git repack -ad && git gc --aggressive --prune=now
+fi
 
